dbdcc321279a49d020db2524474c8f891c993ed0
konukcan <58521958+konukcan@users.noreply.github.com>
2020-01-19 14:34:39 +0100

    Update and rename Code_for_extracting_emotions_from_sentences.md to Code_for_extracting_emotions_from_sentences.py

--- Code_for_extracting_emotions_from_sentences.py
+++ Code_for_extracting_emotions_from_sentences.py
@@ -1,5 +1,5 @@
  ### Here is the code to extract scores of emotions from sentences.
- Reminder:  the arrays after each sentence score emotions in the following order : joy, fear, sadness, anger, surprise, disgust.
+ #Reminder:  the arrays after each sentence score emotions in the following order : joy, fear, sadness, anger, surprise, disgust.
 
  ~~~~ruby
 
@@ -32,23 +32,18 @@ for index, row in feely_data.iterrows():
     feely_dict[row['word']] = list(row[3:10])
 
 
-general = []
+sentences_and_scores = []
 
 for sent in sentences:
     array = []
     for mot in sent.split():
         if mot in feely_dict:
            array.append(feely_dict.get(mot))
-    general.append(sent)
-    general.append([sum(i) for i in zip(*array)])
+    sentences_and_scores.append(sent)
+    sentences_and_scores.append([sum(i) for i in zip(*array)])
 
-for whatever in general[500:600]:
+for element in sentences_and_scores[550:600]:
     print("::: \n")
-    print(whatever)
+    print(element)
     
  ~~~~
- 
-    Les commentaires suivants peuvent être ajoutés :
-    
-    -
-    -
